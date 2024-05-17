d8722737aa624e3b16265440b5e788e27e54950f
Ł Kapkowski <s28002@pjwstk.edu.pl>
2024-05-17 20:52:45 +0200

    dodano 3 zadanie

--- zjazd4/zad3.php
+++ zjazd4/zad3.php
@@ -1,1 +1,30 @@
 <?php
+function generateLinks($filePath) {
+    if (!file_exists($filePath)) {
+        return "File not found.";
+    }
+
+    $file = fopen($filePath, 'r');
+    if (!$file) {
+        return "Unable to open the file.";
+    }
+
+    $linksHtml = "<ul>\n";
+
+    while (($line = fgets($file)) !== false) {
+        $parts = explode(';', trim($line));
+        if (count($parts) == 2) {
+            $url = htmlspecialchars($parts[0]);
+            $description = htmlspecialchars($parts[1]);
+            $linksHtml .= "<li><a href=\"$url\">$description</a></li>\n";
+        }
+    }
+
+    fclose($file);
+    $linksHtml .= "</ul>\n";
+
+    return $linksHtml;
+}
+
+echo generateLinks('links.txt');
+?>
